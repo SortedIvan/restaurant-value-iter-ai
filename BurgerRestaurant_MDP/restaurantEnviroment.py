449b5864e994c1e7ac415401c0000305304cd550
Valeva, Veronika V <i450701>
2022-06-13 18:37:40 +0200

    added probability def()

--- BurgerRestaurant_MDP/restaurantEnviroment.py
+++ BurgerRestaurant_MDP/restaurantEnviroment.py
@@ -2,10 +2,7 @@ from enum import Enum
 from random import randint, choice
 from copy import copy
 
-E, X, O = ' ', 'X', 'O'
-
-
-E, B1 , L , M , B2, START, END = ' ', 'B1', 'L', 'M','B2', 'ST', 'END'
+E, B1, L, M, B2, START, END = ' ', 'B1', 'L', 'M', 'B2', 'ST', 'END'
 
 
 class RestaurantEnvironment():
@@ -18,15 +15,12 @@ class RestaurantEnvironment():
             self.__initial_state[17] = B2
             self.__initial_state[0] = START
             self.__initial_state[24] = END
-            # start with empty board
+            self.__possible_states = []
+            self.playerState = [0, ""]
+            self.reward = 0
         else:
-            self.__initial_state = copy(initial_state)  # copy to prevent aliassing
-        self.__state = self.__initial_state
-        self.__possible_states = []
-        # self.__calculate_possible_states(self.__initial_state)
-        self.currentWord = ""
-        self.curPlayerPos = 0
-
+            self.__initial_state = copy(initial_state)
+            self.__state = self.__initial_state
 
     def reset(self):
         self.__state = self.__initial_state
@@ -41,25 +35,29 @@ class RestaurantEnvironment():
         elif playerPosition == 8:
             return "R"
         elif playerPosition == 17:
-                return "G"
-
+            return "G"
 
     def step(self, playerPosition):
-        self.currentWord += self.calculate_curr_word(playerPosition)
+        if (self.calculate_curr_word(playerPosition)) is not None:
+            self.playerState[1] += self.calculate_curr_word(playerPosition)
+            self.reward += 1
+        else:
+            self.reward -= 0.1
+        self.playerState[0] = playerPosition
         observation = self.__state  # environment is fully observable
-        done = self.get_killed_or_live(self)
+        done = self.get_killed_or_live()
         info = {}  # optional debug info
         return observation, done, info
 
     def render(self):
         BACKGROUND = [
-            ' S │   │   │   │   ',
+            '  │   │   │   │   ',
             '───┼───┼───┼───┼───',
             '   │   │   │ L │   ',
             '───┼───┼───┼───┼───',
-            '   │ B1│   │   │   ',
+            '   │ B│   │   │   ',
             '───┼───┼───┼───┼───',
-            '   │   │ B2│   │   ',
+            '   │   │ B│   │   ',
             '───┼───┼───┼───┼───',
             '   │ M │   │   │ E '
         ]
@@ -78,43 +76,67 @@ class RestaurantEnvironment():
     # public functions for agent to calculate optimal policy
     # =========================================================
 
-    # def get_possible_states(self):
-    #     return self.__possible_states
-
     def get_possible_actions(self):
-        if self.curPlayerPos in (6, 7, 8,11,12,13,16,17,18):
-            return [self.curPlayerPos + 1, self.curPlayerPos - 1, self.curPlayerPos - 5, self.curPlayerPos + 5]
-        elif self.curPlayerPos in (1,2,3):
-            return [self.curPlayerPos + 1, self.curPlayerPos - 1, self.curPlayerPos + 5]
-        elif self.curPlayerPos in (5, 10, 15):
-            return [self.curPlayerPos + 5, self.curPlayerPos -5, self.curPlayerPos + 1]
-        elif self.curPlayerPos in (9, 14, 19):
-            return [self.curPlayerPos + 5, self.curPlayerPos - 5, self.curPlayerPos - 1]
-        elif self.curPlayerPos in (21, 22, 23):
-            return [self.curPlayerPos + 1, self.curPlayerPos - 5, self.curPlayerPos - 1]
-        elif self.curPlayerPos == 0:
-            return [self.curPlayerPos + 1, self.curPlayerPos + 5]
-        elif self.curPlayerPos == 4:
-            return [self.curPlayerPos - 1, self.curPlayerPos + 5]
-        elif self.curPlayerPos == 20:
-            return [self.curPlayerPos + 1, self.curPlayerPos - 5]
-        elif self.curPlayerPos == 24:
+        if self.playerState[0] in (6, 7, 8, 11, 12, 13, 16, 17, 18):
+            return [self.playerState[0] + 1, self.playerState[0] - 1, self.playerState[0] - 5, self.playerState[0] + 5]
+        elif self.playerState[0] in (1, 2, 3):
+            return [self.playerState[0] + 1, self.playerState[0] - 1, self.playerState[0] + 5]
+        elif self.playerState[0] in (5, 10, 15):
+            return [self.playerState[0] + 5, self.playerState[0] - 5, self.playerState[0] + 1]
+        elif self.playerState[0] in (9, 14, 19):
+            return [self.playerState[0] + 5, self.playerState[0] - 5, self.playerState[0] - 1]
+        elif self.playerState[0] in (21, 22, 23):
+            return [self.playerState[0] + 1, self.playerState[0] - 5, self.playerState[0] - 1]
+        elif self.playerState[0] == 0:
+            return [self.playerState[0] + 1, self.playerState[0] + 5]
+        elif self.playerState[0] == 4:
+            return [self.playerState[0] - 1, self.playerState[0] + 5]
+        elif self.playerState[0] == 20:
+            return [self.playerState[0] + 1, self.playerState[0] - 5]
+        elif self.playerState[0] == 24:
             return 24
 
+    def get_step_probability(self, new_state, inventory):
+        new_states = self.get_possible_actions()
+        if new_states.__contains__(new_state):
+            if new_state == 8:
+                if self.playerState[1] == "BU" and inventory == "R":      # HUGE QUESTION ? -> self.playerState[1] == "" and inventory == "R" return 1/0?
+                    return 1
+                else:
+                    return 0
+            elif new_state == 11:
+                if self.playerState[1] == "" and inventory == "B":
+                    return 1
+                else:
+                    return 0
+            elif new_state == 17:
+                if self.playerState[1] == "BUR" and inventory == "G":
+                    return 1
+                else:
+                    return 0
+            elif new_state == 21:
+                    if self.playerState[1] == "B" and inventory == "U":
+                        return 1
+                    else:
+                        return 0
+            elif self.playerState[1] == inventory :
+                return 1
+            else:
+                return 0
+        else:
+            return 0
 
     def get_killed_or_live(self):  # B
         # Reward R(s) for every possible state
         # Current word must be stored somewhere else
-          # B, BU, BUR, BURG
-        if self.currentWord != "B" or "BU" or "BUR" or "BURG":
+        # B, BU, BUR, BURG
+        if self.playerState[1] != "B" or "BU" or "BUR" or "BURG":
             return False
         return True
 
     def get_transition_prob(self, action, new_state, old_state=None):
-        # if old_state is None:
-        #     old_state = self.__state
-        # # returns the Transition Probability P(s'| s, a)
-        # # with s = old_state, a = action and s' = new_state
+        # returns the Transition Probability P(s'| s, a)
+        # with s = old_state, a = action and s' = new_state
 
         # # if the game is over, no transition can take place
         # if self.is_done(old_state):
@@ -151,10 +173,13 @@ class Game():
     mdp = RestaurantEnvironment()
     mdp.reset()
     mdp.render()
-    mdp.curPlayerPos = 0
-    print(mdp.curPlayerPos)
-    print(mdp.get_possible_actions())
+    print(mdp.step(11))
+    print(mdp.step(21))
+    # print(mdp.step(8))
+    print(mdp.step(16))
+    print(mdp.playerState[0], mdp.playerState[1])
+    print(mdp.get_step_probability(17,"G"))
     print('possible (internal) game states:')
-    # mdp.get_possible_states()
+
 
 game = Game()
